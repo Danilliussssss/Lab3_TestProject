f7c696daa758f3ca3a80a3876a70f6a6b0bdf5b2
Danilka <danil_ilin_2016@bk.ru>
2025-03-24 13:51:47 +0700

    test answer_on_message method successfully

--- test.py
+++ test.py
@@ -2,13 +2,18 @@ import unittest
 
 
 class Console_friend:
-    pass
+    def answer_on_message(self,msg): #todo
+        return "Ответ на сообщение"
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         friend_obj = Console_friend()
-        self.assertIsNotNone(friend_obj, "Объект класса не создан!")  # add assertion here
+        self.assertIsNotNone(friend_obj, "Объект класса не создан!")
+    def test_answer_message(self):
+        friend_obj = Console_friend()
+        self.assertEqual("Ответ на сообщение",friend_obj.answer_on_message("Сообщение"),"Совпадение")
+
 
 
 
